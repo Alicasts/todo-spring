eb0b7a6b6452ffb9e98bd686483744d18d4cda58
Ali <ali.cast.santos@gmail.com>
2023-10-09 12:15:29 -0300

    Initialize Spring Web project

---
+++ src/test/java/com/acast/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acast.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
